889af0f09569a74ecd7b327b487e4da6254ef037
Ahmed Ali <ahmedaliaay@gmail.com>
2021-12-11 20:24:26 +0200

    Created FoodApiClient

--- app/src/main/java/com/candybytes/taco/data/api/food/FoodApiClient.kt
+++ app/src/main/java/com/candybytes/taco/data/api/food/FoodApiClient.kt
@@ -1,4 +1,13 @@
 package com.candybytes.taco.data.api.food
 
-class FoodApiClient {
+import com.candybytes.taco.data.api.TacoService
+import com.candybytes.taco.domain.model.Food
+import javax.inject.Inject
+
+class FoodApiClient @Inject constructor(
+    private val client: TacoService,
+) {
+    suspend fun getAllFood(): List<Food> {
+        return client.getAllFoodAsync().map { it.toModel() }
+    }
 }
